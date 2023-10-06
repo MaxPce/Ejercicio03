7c9e0cbd43c0c8d6f06210cbad1fcb571b550ff9
Alumno <Alumno@LAB1509-25>
2023-10-06 16:55:26 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03/Ejercicio03/ImageResourceExtension.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+
+namespace Ejercicio03
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public partial class ImageResourceExtension : ContentPage
+    {
+        public ImageResourceExtension()
+        {
+            InitializeComponent();
+        }
+    }
+}
